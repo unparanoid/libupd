06752d5bb2df08954347c1ad2d62d654b01f7116
falsycat <me@falsy.cat>
2021-08-17 00:00:00 +0000

    Fixes refcnt management in a protocol implementation.

--- libupd/proto.h
+++ libupd/proto.h
@@ -2,13 +2,18 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
 
 #include <hedley.h>
 #include <msgpack.h>
 
 #include <libupd.h>
 
+#include "pathfind.h"
 #include "str.h"
+
+
+#define UPD_PROTO_PARSE_HOLD_MAX 4
 
 
 typedef struct upd_proto_msg_t   upd_proto_msg_t;
@@ -61,6 +66,8 @@ struct upd_proto_parse_t {
 
   const char* err;
 
+  upd_file_t* hold[UPD_PROTO_PARSE_HOLD_MAX];
+
   void* udata;
   void
   (*cb)(
@@ -81,14 +88,27 @@ upd_proto_parse_with_dup(
   const upd_proto_parse_t* src);
 
 
-#define upd_proto_parse_unref_(par) do {  \
-    if (HEDLEY_UNLIKELY(--par->refcnt == 0)) {  \
-      par->cb(par);  \
-    }  \
-  } while (0)
+static inline
+void
+upd_proto_parse_hold_(
+  upd_proto_parse_t* par,
+  upd_file_t*        f);
 
-#include "proto/encoder.h"
-#include "proto/object.h"
+static inline
+void
+upd_proto_parse_unref_(
+  upd_proto_parse_t* par);
+
+
+static inline
+void
+upd_proto_parse_encoder_(
+  upd_proto_parse_t* par);
+
+static inline
+void
+upd_proto_parse_object_(
+  upd_proto_parse_t* par);
 
 
 static inline void upd_proto_parse(upd_proto_parse_t* par) {
@@ -135,7 +155,7 @@ static inline void upd_proto_parse(upd_proto_parse_t* par) {
       }
       msg->cmd = c->i;
 
-      upd_proto_parse_encoder(par);
+      upd_proto_parse_encoder_(par);
       goto EXIT;
     }
   }
@@ -158,7 +178,7 @@ static inline void upd_proto_parse(upd_proto_parse_t* par) {
       }
       msg->cmd = c->i;
 
-      upd_proto_parse_object(par);
+      upd_proto_parse_object_(par);
       goto EXIT;
     }
   }
@@ -183,4 +203,154 @@ static inline bool upd_proto_parse_with_dup(const upd_proto_parse_t* src) {
 }
 
 
-#undef upd_proto_parse_unref_
+static inline void upd_proto_parse_hold_(
+    upd_proto_parse_t* par, upd_file_t* f) {
+  for (size_t i = 0; i < UPD_PROTO_PARSE_HOLD_MAX; ++i) {
+    if (HEDLEY_LIKELY(par->hold[i] == NULL)) {
+      par->hold[i] = f;
+      upd_file_ref(f);
+      return;
+    }
+  }
+  assert(false);
+  HEDLEY_UNREACHABLE();
+}
+
+static inline void upd_proto_parse_unref_(upd_proto_parse_t* par) {
+  if (HEDLEY_LIKELY(--par->refcnt)) {
+    return;
+  }
+
+  upd_file_t* hold[UPD_PROTO_PARSE_HOLD_MAX];
+  memcpy(hold, par->hold, sizeof(hold));
+
+  par->cb(par);  /* par is freed and not available anymore */
+
+  for (size_t i = 0; i < UPD_PROTO_PARSE_HOLD_MAX; ++i) {
+    if (HEDLEY_UNLIKELY(par->hold[i])) {
+      upd_file_unref(par->hold[i]);
+    }
+  }
+}
+
+
+static inline void upd_proto_encoder_frame_file_pathfind_cb_(
+    upd_pathfind_t* pf) {
+  upd_proto_parse_t* par = pf->udata;
+  upd_iso_t*         iso = pf->iso;
+  upd_proto_msg_t*   msg = &par->msg;
+
+  upd_file_t* target = pf->len? NULL: pf->base;
+  upd_iso_unstack(iso, pf);
+
+  if (HEDLEY_UNLIKELY(target == NULL)) {
+    par->err = "file not found";
+    goto EXIT;
+  }
+  msg->encoder_frame.file = target;
+  upd_proto_parse_hold_(par, target);
+
+EXIT:
+  upd_proto_parse_unref_(par);
+}
+static inline void upd_proto_parse_encoder_(upd_proto_parse_t* par) {
+  upd_iso_t*       iso = par->iso;
+  upd_proto_msg_t* msg = &par->msg;
+
+  switch (msg->cmd) {
+  case UPD_PROTO_ENCODER_INFO:
+    break;
+
+  case UPD_PROTO_ENCODER_INIT:
+    break;
+
+  case UPD_PROTO_ENCODER_FRAME: {
+    if (HEDLEY_UNLIKELY(msg->param == NULL)) {
+      par->err = "invalid param";
+      return;
+    }
+
+    uintmax_t                 file_i = 0;
+    const msgpack_object_str* file_s = NULL;
+    const char* invalid =
+      upd_msgpack_find_fields(msg->param, (upd_msgpack_field_t[]) {
+          { .name = "file", .ui = &file_i, .str = &file_s, .required = true, },
+          { NULL, },
+        });
+    if (HEDLEY_UNLIKELY(invalid)) {
+      par->err = "invalid param";
+      return;
+    }
+
+    if (file_s) {
+      ++par->refcnt;
+      const bool ok = upd_pathfind_with_dup(&(upd_pathfind_t) {
+          .iso   = iso,
+          .path  = (uint8_t*) file_s->ptr,
+          .len   = file_s->size,
+          .udata = par,
+          .cb    = upd_proto_encoder_frame_file_pathfind_cb_,
+        });
+      if (HEDLEY_UNLIKELY(!ok)) {
+        --par->refcnt;
+        par->err = "subreq failure";
+        return;
+      }
+    } else {
+      upd_file_t* target = upd_file_get(iso, file_i);
+      if (HEDLEY_UNLIKELY(target == NULL)) {
+        par->err = "file not found";
+        return;
+      }
+      msg->encoder_frame.file = target;
+      upd_proto_parse_hold_(par, target);
+    }
+  } break;
+
+  case UPD_PROTO_ENCODER_FINALIZE:
+    break;
+
+  default:
+    HEDLEY_UNREACHABLE();
+    assert(false);
+  }
+}
+
+static inline void upd_proto_parse_object_(upd_proto_parse_t* par) {
+  upd_proto_msg_t* msg = &par->msg;
+
+  switch (msg->cmd) {
+  case UPD_PROTO_OBJECT_LOCK:
+    return;
+  case UPD_PROTO_OBJECT_LOCKEX:
+    return;
+
+  case UPD_PROTO_OBJECT_UNLOCK:
+    return;
+
+  case UPD_PROTO_OBJECT_GET:
+  case UPD_PROTO_OBJECT_SET: {
+    if (msg->param == NULL) {
+      return;
+    }
+    const msgpack_object_array* path  = NULL;
+    const msgpack_object*       value = NULL;
+    const char* invalid =
+      upd_msgpack_find_fields(msg->param, (upd_msgpack_field_t[]) {
+          { .name = "path",  .array = &path,  },
+          { .name = "value", .any   = &value, },
+          { NULL, },
+        });
+    if (HEDLEY_UNLIKELY(invalid)) {
+      par->err = "invalid param";
+      return;
+    }
+    msg->object.path  = path;
+    msg->object.value = value;
+  } return;
+
+  default:
+    assert(false);
+    HEDLEY_UNREACHABLE();
+  }
+}
